2e14a585e01c29da3def2713d6da3782fc09310a
infermi <80839246+infermi@users.noreply.github.com>
2023-06-23 17:32:33 +0300

    git push

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: 'https://infermi.github.io',
-    base: '/ar',
+    site: 'https://pastoralit.co.il',
 });
